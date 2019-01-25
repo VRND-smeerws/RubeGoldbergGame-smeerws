ef6106b93c0e0bee01659db8b69e510976c519c9
smeerws <hello@smeerws.at>
2019-01-25 12:18:39 +0100

    update

---
+++ Assets/MyGame/Scripts/ControllerInputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputManager : MonoBehaviour {
+
+    //Activates by pressing the left Touchpad
+    //Shows an aim indicator(such as a laser) coming from the left controller while the left Touchpad is held down
+    //Shows an indicator of where the player will teleport while the left Touchpad is held down
+    //Moves the player to the aimed-at position on left Touchpad release
+    //Review the earlier course materials if you’re uncertain how to do these steps
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
